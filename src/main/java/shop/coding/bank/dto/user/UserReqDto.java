58dde409cbdf59c9e508ece9a4698aef54a3a2b8
Beginner <dkstmdwn17@naver.com>
2023-03-10 01:21:49 +0900

    feature: 회원가입 시 공백이 입력되었을 때 유효성 검사 로직 추가
    `UserReqDto` 에 @Notnull을 붙여주고,
    `UserController`에 join의 파라미터에
    `@Valid`, `BindingResult`를 추가해주고
    유효성 검사에 에러가 발생했을 경우
    BindingResult 객체에 에러가 담겨
    이를 처리해주는 로직을 작성하였다.

--- src/main/java/shop/coding/bank/dto/user/UserReqDto.java
+++ src/main/java/shop/coding/bank/dto/user/UserReqDto.java
@@ -6,15 +6,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import shop.coding.bank.domain.user.User;
 import shop.coding.bank.domain.user.UserEnum;
 
+import javax.validation.constraints.NotEmpty;
+
 public class UserReqDto {
 
     @Getter
     @Setter
     public static class JoinReqDto {
-        // 유효성 검사
+        @NotEmpty // null이거나, 공백일 수 없다.
         private String username;
+        @NotEmpty
         private String password;
+        @NotEmpty
         private String email;
+        @NotEmpty
         private String fullname;
 
         public User toEntity(BCryptPasswordEncoder passwordEncoder) {
