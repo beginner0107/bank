5d424d3298f53a40c4a895a1ef3fe6ee74421710
Beginner <dkstmdwn17@naver.com>
2023-03-10 11:26:28 +0900

    feature: 회원가입 컨트롤러 유효성 검사 AOP적용
    `CustomExceptionHandler`: `CustomValidationException`이 던져지면
    Json 형태의 에러 메시지를 리턴한다.
    `CustomValidationException`: 회원가입 유효성 검사가
    실패하면 던져질 Exception이다.
    `CustomValidationAdvice`: AOP기능을 활용
    Post, Put 요청시 `@Around`를 통해
    joinPoint를 전후 제어를 한다.
    `BindingResult`에 error가 존재할 경우에만
    유효성검사 실패했다는 메시지를
    `CustomValidationException`에 넘겨준다

--- src/main/java/shop/coding/bank/handler/CustomExceptionHandler.java
+++ src/main/java/shop/coding/bank/handler/CustomExceptionHandler.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import shop.coding.bank.dto.ResponseDto;
 import shop.coding.bank.handler.ex.CustomApiException;
+import shop.coding.bank.handler.ex.CustomValidationException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -19,5 +20,11 @@ public class CustomExceptionHandler {
     public ResponseEntity<?> apiException(CustomApiException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), null), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(CustomValidationException.class)
+    public ResponseEntity<?> validationApiException(CustomValidationException e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(new ResponseDto<>(-1, e.getMessage(), e.getErrorMap()), HttpStatus.BAD_REQUEST);
     }
 }
