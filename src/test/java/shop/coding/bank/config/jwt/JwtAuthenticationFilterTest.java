63110710a2c5adcbed364d386975b4b6d647b5ac
Beginner <dkstmdwn17@naver.com>
2023-03-12 00:16:43 +0900

    test: successfulAuthentication 테스트
    profile설정을하기 위해 `@ActiveProfiles("test")`
    를 넣어주었다.
    Login 인증을 하기 위해
    @BeforeEach의 setUp()메서드를 통해
    새로운 유저를 저장시킨다.
    이후 테스트에서 로그인을 할 때
    사용하게 되며, 정상적으로 로그인이 되었을 시
    응답 status가 200인지,
    jwtToken이 넘어왔는지 테스트를 하였다.

---
+++ src/test/java/shop/coding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -0,0 +1,70 @@
+package shop.coding.bank.config.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import shop.coding.bank.config.dummy.DummyObject;
+import shop.coding.bank.domain.user.UserRepository;
+import shop.coding.bank.dto.user.UserReqDto.LoginReqDto;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+class JwtAuthenticationFilterTest extends DummyObject {
+
+    @Autowired
+    private ObjectMapper om;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        userRepository.save(newUser("ssar", "쌀"));
+    }
+
+    @Test
+    public void successfulAuthentication_test() throws Exception {
+        // Given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("1234");
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("테스트 : " + requestBody);
+
+        // When
+        ResultActions resultActions = mvc.perform(post("/api/login").content(requestBody)
+                                         .contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("테스트 : " + responseBody);
+        System.out.println("테스트 : " + jwtToken);
+
+        // Then
+        resultActions.andExpect(status().isOk());
+        assertNotNull(jwtToken);
+        assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
+        resultActions.andExpect(jsonPath("$.data.username").value("ssar"));
+    }
+
+    @Test
+    public void unsuccessfulAuthentication_test() throws Exception {
+
+    }
+}
