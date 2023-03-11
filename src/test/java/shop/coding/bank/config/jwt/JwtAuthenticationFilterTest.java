51d9ff41ab2b240eaa784e0a55dc0f49c59621db
Beginner <dkstmdwn17@naver.com>
2023-03-12 00:25:04 +0900

    test: unsuccessfulAuthentication 테스트
    인증에 실패했을때 실행될
    unsuccessfulAuthentication() 메서드를
    테스트하였다.
    401상태 코드를 가지고 있어야 한다.
    `@Transaction` 어노테이션을 붙여
    각각의 테스트가 끝난 후 Rollback이 되도록
    처리하였다.

--- src/test/java/shop/coding/bank/config/jwt/JwtAuthenticationFilterTest.java
+++ src/test/java/shop/coding/bank/config/jwt/JwtAuthenticationFilterTest.java
@@ -11,17 +11,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 import shop.coding.bank.config.dummy.DummyObject;
 import shop.coding.bank.domain.user.UserRepository;
 import shop.coding.bank.dto.user.UserReqDto.LoginReqDto;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
@@ -65,6 +66,22 @@ class JwtAuthenticationFilterTest extends DummyObject {
 
     @Test
     public void unsuccessfulAuthentication_test() throws Exception {
+        // Given
+        LoginReqDto loginReqDto = new LoginReqDto();
+        loginReqDto.setUsername("ssar");
+        loginReqDto.setPassword("12345");
+        String requestBody = om.writeValueAsString(loginReqDto);
+        System.out.println("테스트 : " + requestBody);
 
+        // When
+        ResultActions resultActions = mvc.perform(post("/api/login").content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        String jwtToken = resultActions.andReturn().getResponse().getHeader(JwtVO.HEADER);
+        System.out.println("테스트 : " + responseBody);
+        System.out.println("테스트 : " + jwtToken);
+
+        // Then
+        resultActions.andExpect(status().isUnauthorized());
     }
 }
