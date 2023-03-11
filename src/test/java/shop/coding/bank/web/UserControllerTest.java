8b58b3c6bafd1f82ac1102a348302f5f6f3e66c4
Beginner <dkstmdwn17@naver.com>
2023-03-12 00:50:02 +0900

    test: `UserControllerTest`에서 롤백이 되지 않는 부분 수정
    테스트가 끝난 후 rollback을 해야
    다른 테스트에 영향을 끼치지 않는
    독립적인 테스트가 된다.
    `@Transaction` 어노테이션을 붙여
    해결하였다.

--- src/test/java/shop/coding/bank/web/UserControllerTest.java
+++ src/test/java/shop/coding/bank/web/UserControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 import shop.coding.bank.config.dummy.DummyObject;
 import shop.coding.bank.domain.user.UserRepository;
 import shop.coding.bank.dto.user.UserReqDto.JoinReqDto;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 class UserControllerTest extends DummyObject {
