3c008ec8c60ae17d253883f060daa1b826795639
Beginner <dkstmdwn17@naver.com>
2023-03-13 14:20:47 +0900

    feature: 본인계좌 목록보기 서비스 구현
    1. userId를 통해 사용자가 존재하는지 확인한다.
    2. findByUser_id를 통해 본인의 계좌 목록들을 불러온다.
    3. 컨트롤러에 보내줄 때 `AccountListRespDto`로 보내준다.
    (fullname과 계좌목록들)
    
    - 계좌 목록을 가져올 때 User 객체를 조인하지 않는 이유:
    userId를 통해 유저 객체를 가져오고,
    이용하는 것은 fullname 뿐,
    그렇기에 조인해서 가져올 필요가 없다.
    (지연로딩 사용)

--- src/main/java/shop/coding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/coding/bank/domain/account/AccountRepository.java
@@ -2,10 +2,15 @@ package shop.coding.bank.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     // select * from account where number = :number
     // checkpoint : 리팩토링 해야함 (계좌 소유자 확인시에 쿼리가 두번 나가기 때문에 join fetch)
     Optional<Account> findByNumber(Long number);
+
+    // jpa query method
+    // select * from account where user_id = :id
+    List<Account> findByUser_id(Long id);
 }
