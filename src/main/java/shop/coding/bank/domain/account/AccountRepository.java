9a5fd3a34a123f0b45fd984abc6ba9a0d7c5f777
Beginner <dkstmdwn17@naver.com>
2023-03-13 09:39:46 +0900

    feature: 계좌 등록 서비스 구현
    계좌 번호로 계좌를 찾아오는
    `findByNumber` query Method를 사용
    계좌 번호를 20자 -> 4자로 변경
    계좌를 등록할 때
    1. 회원인지 확인
    2. 계좌가 DB에 있는지 중복 체크
    3. 계좌 등록
    `Dto`를 서비스 레이어에 만들었는데,
    추후에 리팩토링 예정

--- src/main/java/shop/coding/bank/domain/account/AccountRepository.java
+++ src/main/java/shop/coding/bank/domain/account/AccountRepository.java
@@ -2,5 +2,10 @@ package shop.coding.bank.domain.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    // select * from account where number = :number
+    // checkpoint : 리팩토링 해야함
+    Optional<Account> findByNumber(Long number);
 }
