1911f37d445e24332e4a2aad476cadcd51d1f1e8
Beginner <dkstmdwn17@naver.com>
2023-03-13 21:37:11 +0900

    test: 계좌 삭제 컨트롤러를 테스트
    테스트 시에는 insert 한것들이 전부 PC에 올라감(영속화)
    영속화 된것들을 초기화 해주는 것이 개발 모드와 동일한 환경으로 테스트를 할 수 있게 해준다.
    최초 select는 쿼리가 발생하지만 PC에 있으면 1차 캐시를 함.
    Lazy 로딩은 쿼리도 발생안함 - PC에 있다면
    Lazy 로딩할 때 PC 없다면 쿼리가 발생함.

--- src/test/java/shop/coding/bank/web/AccountControllerTest.java
+++ src/test/java/shop/coding/bank/web/AccountControllerTest.java
@@ -17,12 +17,17 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import shop.coding.bank.config.auth.LoginUser;
 import shop.coding.bank.config.dummy.DummyObject;
+import shop.coding.bank.domain.account.Account;
+import shop.coding.bank.domain.account.AccountRepository;
 import shop.coding.bank.domain.user.User;
 import shop.coding.bank.domain.user.UserRepository;
 import shop.coding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
+import shop.coding.bank.handler.ex.CustomApiException;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
@@ -39,10 +44,19 @@ class AccountControllerTest extends DummyObject {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private EntityManager em;
 
     @BeforeEach
     public void setUp() {
         User ssar = userRepository.save(newUser("ssar", "쌀"));
+        User cos = userRepository.save(newUser("cos", "코스"));
+        Account ssarAccount1 = accountRepository.save(newAccount(1111L, ssar));
+        Account cosAccount1 = accountRepository.save(newAccount(2222L, cos));
+        em.clear();
     }
 
     // jwt token -> 인증필터 -> 시큐리티 세션생성
@@ -81,5 +95,31 @@ class AccountControllerTest extends DummyObject {
 
         // Then
         resultActions.andExpect(status().isOk());
+    }
+
+    /**
+     * 테스트 시에는 insert 한것들이 전부 PC에 올라감(영속화)
+     * 영속화 된것들을 초기화 해주는 것이 개발 모드와 동일한 환경으로 테스트를 할 수 있게 해준다.
+     * 최초 select는 쿼리가 발생하지만 PC에 있으면 1차 캐시를 함.
+     * Lazy 로딩은 쿼리도 발생안함 - PC에 있다면
+     * Lazy 로딩할 때 PC 없다면 쿼리가 발생함.
+     */
+    @WithUserDetails(value = "ssar", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @Test
+    void deleteAccount_test() throws Exception {
+        // Given
+        Long number = 1111L;
+
+        // When
+        ResultActions resultActions = mvc
+                .perform(delete("/api/s/account/" + number));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println("테스트 : " + responseBody);
+
+        // Then
+        // Junit 테스트에서 delete 쿼리는 DB관련(DML) 가장 마지막에 실행되면 발동안됨.
+        assertThrows(CustomApiException.class, () -> accountRepository.findByNumber(number).orElseThrow(
+                () -> new CustomApiException("계좌를 찾을 수 없습니다."))
+        );
     }
 }
