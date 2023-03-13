5529d3797c3034c901873d0f49f56b8e22c44105
Beginner <dkstmdwn17@naver.com>
2023-03-14 00:56:12 +0900

    test: Jwt 토큰 만료시간 버그 고침
    현재 JwtProcessTest에서
    jwtToken을 생성하는 과정을 수동으로
    처리해줬기 때문에
    언젠가 만료된다. 그렇기 때문에
    jwtToken을 생성하는 부분을 메서드로
    빼주어서 재사용이 가능하게 만들어 주었다.

--- src/test/java/shop/coding/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/shop/coding/bank/config/jwt/JwtProcessTest.java
@@ -11,6 +11,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JwtProcessTest {
 
+    private String createToken() {
+        // Given
+        User user = User.builder().id(1L).role(UserEnum.CUSTOMER).build();
+        LoginUser loginUser = new LoginUser(user);
+
+        // When
+        String jwtToken = JwtProcess.create(loginUser);
+        return jwtToken;
+    }
+
     @Test
     void create_Test() throws Exception {
         // Given
@@ -18,7 +28,7 @@ class JwtProcessTest {
         LoginUser loginUser = new LoginUser(user);
 
         // When
-        String jwtToken = JwtProcess.create(loginUser);
+        String jwtToken = createToken();
         System.out.println("테스트 : " + jwtToken);
 
         // Then
@@ -28,10 +38,11 @@ class JwtProcessTest {
     @Test
     void verify_test() throws Exception {
         // Given
-        String jwkToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6MTY3OTEzMjQ3N30.tVOnOxZgR8jMY0-x_th6Va7G9W4tbD9L8tUomkJZBi66gWbV7ZMY_vogFY1tumko_nmNO2St56-AI9VHdAmgLw";
+        String token = createToken();
+        String jwtToken = token.replace(JwtVO.TOKEN_PREFIX, "");
 
         // When
-        LoginUser loginUser = JwtProcess.verify(jwkToken);
+        LoginUser loginUser = JwtProcess.verify(jwtToken);
         System.out.println("테스트 : " + loginUser.getUser().getId());
         System.out.println("테스트 : " + loginUser.getUser().getRole().name());
 
