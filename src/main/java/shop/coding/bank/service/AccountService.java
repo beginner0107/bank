8a4f3b7b247c9b72623c0ae7482f8ecfa1b283bd
Beginner <dkstmdwn17@naver.com>
2023-03-14 13:14:03 +0900

    feature: 계좌입금 서비스 구현
    계좌 입금 서비스
    1. 0원 체크
    2. 입금계좌가 존재하는지 확인
    3. 입금(domain영역에서 더티 체킹)
    4. 거래 내역 남기기(transactionRepository.save)
    
    - return 값은 Dto를 만들어 계좌ID, 계좌번호, TransactionDto를 리턴한다
    - depositAccountBalance의 경우 입금한 후 그 해당 계좌의 금액인데,
    클라이언트에게 보여줄 필요가 없고,
    테스트를 작성할 때만 사용할 것이기 때문에
    `@JosnIgnore` 어노테이션을 붙여두었다

--- src/main/java/shop/coding/bank/service/AccountService.java
+++ src/main/java/shop/coding/bank/service/AccountService.java
@@ -1,17 +1,28 @@
 package shop.coding.bank.service;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.coding.bank.domain.account.Account;
 import shop.coding.bank.domain.account.AccountRepository;
+import shop.coding.bank.domain.transaction.Transaction;
+import shop.coding.bank.domain.transaction.TransactionEnum;
+import shop.coding.bank.domain.transaction.TransactionRepository;
 import shop.coding.bank.domain.user.User;
 import shop.coding.bank.domain.user.UserRepository;
 import shop.coding.bank.dto.account.AccountReqDto.AccountSaveReqDto;
 import shop.coding.bank.dto.account.AccountRespDto.AccountListRespDto;
 import shop.coding.bank.dto.account.AccountRespDto.AccountSaveRespDto;
 import shop.coding.bank.handler.ex.CustomApiException;
+import shop.coding.bank.util.CustomDateUtil;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +33,7 @@ public class AccountService {
 
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
+    private final TransactionRepository transactionRepository;
 
     public AccountListRespDto 계좌목록보기_유저별(Long userId) {
         User userPS = userRepository.findById(userId).orElseThrow(
@@ -53,6 +65,7 @@ public class AccountService {
         return new AccountSaveRespDto(accountPS);
     }
 
+
     @Transactional
     public void 계좌삭제(Long number, Long userId) {
         // 1. 계좌 확인
@@ -65,5 +78,92 @@ public class AccountService {
 
         // 3. 계좌 삭제
         accountRepository.deleteById(accountPS.getId());
+    }
+
+    @Transactional
+    public AccountDepositRespDto 계좌입급(AccountDepositReqDto accountDepositReqDto) { // ATM -> 누군가의 계좌
+        // 0원 체크
+        if (accountDepositReqDto.getAmount() <= 0L) {
+            throw new CustomApiException("0원 이하의 금액을 입금할 수 없습니다");
+        }
+        // 임금계좌 확인
+        Account depositAccountPS = accountRepository.findByNumber(accountDepositReqDto.getNumber())
+                .orElseThrow(
+                        () -> new CustomApiException("계좌를 찾을 수 없습니다")
+                );
+
+        // 입금 (해당 계좌 balance 조정 - update문 - 더티체킹)
+        depositAccountPS.deposit(accountDepositReqDto.getAmount());
+
+        // 거래내역 남기기
+        Transaction transaction = Transaction.builder()
+                .depositAccount(depositAccountPS) // 입금계좌ID
+                .withdrawAccount(null) // 출금 X -> null
+                .depositAccountBalance(depositAccountPS.getBalance()) // 입금한 후 금액
+                .withdrawAccountBalance(null) // 출금 X -> null
+                .amount(accountDepositReqDto.getAmount()) // 입금할 금액
+                .gubun(TransactionEnum.DEPOSIT) // 입금 -> DEPOSIT
+                .sender("ATM") 
+                .receiver(depositAccountPS.getNumber() + "") // 입금받는 사람의 계좌번호
+                .tel(accountDepositReqDto.getTel()) // 입금하는 사람의 전화번호
+                .build();
+
+        Transaction transactionPS = transactionRepository.save(transaction);
+        return new AccountDepositRespDto(depositAccountPS, transactionPS);
+    }
+
+    @Setter
+    @Getter
+    public static class AccountDepositRespDto {
+        private Long id; // 계좌 ID
+        private Long number; // 계좌번호
+        private TransactionDto transaction;
+
+        public AccountDepositRespDto(Account account, Transaction transaction) {
+            this.id = account.getId();
+            this.number = account.getNumber();
+            this.transaction = new TransactionDto(transaction);
+        }
+
+        @Getter
+        @Setter
+        public class TransactionDto {
+            private Long id;
+            private String gubun;
+            private String sender;
+            private String receiver;
+            private Long amount;
+            @JsonIgnore
+            private Long depositAccountBalance; // 클라이언트에게 전달 X -> 서비스 단에서 테스트 용도
+            private String tel;
+            private String createdAt;
+
+            public TransactionDto(Transaction transaction) {
+                this.id = transaction.getId();
+                this.gubun = transaction.getGubun().getValue();
+                this.sender = transaction.getSender();
+                this.receiver = transaction.getReceiver();
+                this.amount = transaction.getAmount();
+                this.depositAccountBalance = transaction.getDepositAccountBalance();
+                this.tel = transaction.getTel();
+                this.createdAt = CustomDateUtil.toStringFormat(transaction.getCreatedAt());
+            }
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class AccountDepositReqDto {
+        @NotNull
+        @Digits(integer = 4, fraction = 4)
+        private Long number;
+        @NotNull
+        private Long amount;
+        @NotEmpty
+        @Pattern(regexp = "DEPOSIT")
+        private String gubun;
+        @NotEmpty
+        @Pattern(regexp = "^[0-9]{11}")
+        private String tel;
     }
 }
